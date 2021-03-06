c523cba8b74afe8bf9fd8ad26ba4562aafd95573
reyes <reyes@LAPTOP-SOI4FI73>
2021-03-06 10:58:07 -0600

    version 1

---
+++ src/primer/Primer.java
@@ -0,0 +1,21 @@
+/*
+Reyes Baltazar Baltazar. Matricula: 2022705
+Licenciatura en Sistemas Computacionales
+ */
+package primer;
+
+/**
+ *
+ * @author reyes
+ */
+public class Primer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola");
+    }
+    
+}
